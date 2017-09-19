4fa218bd4dbc0f45f418fc89a6b54d5a38997501
Klaus Landsdorf <klaus@bianco-royal.de>
2017-09-19 21:24:15 +0200

    update to get it work to install everywehre

--- objectid.js
+++ objectid.js
@@ -7,15 +7,22 @@ module.exports = function(RED) {
     this.name = config.name;
     this.property = config.property || 'payload';
 
-    this.on('input', (msg)=> {
+    this.on('input', msg=> {
       var value;
 
       try {
         value = RED.util.getMessageProperty(msg, this.property);
       }
-      catch (e) {}
+      catch (e) {
+        node.error(e, msg);
+      }
 
-      value = new ObjectID(value);
+      if (Array.isArray(value)) {
+        value = value.map(value=> new ObjectID(value));
+      }
+      else {
+        value = new ObjectID(value);
+      }
 
       RED.util.setMessageProperty(msg, this.property, value, true);
 
